79c82f92d0f0d67c0fde234a5b13fbcc5238c1a6
idib <id.ib.mail@gmail.com>
2019-05-26 02:58:46 +0300

    add edit/delete doc

--- src/main/java/com/nwchat/сontroller/DocController.java
+++ src/main/java/com/nwchat/сontroller/DocController.java
@@ -4,11 +4,13 @@ import com.nwchat.model.Doc;
 import com.nwchat.repository.DocsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -43,6 +45,48 @@ public class DocController {
 
 		model.addObject("doc", doc);
 		model.setViewName("doc/show");
+		return model;
+	}
+
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+	public ModelAndView create(@Valid Doc doc, BindingResult result) {
+		ModelAndView model = new ModelAndView();
+
+		if (result.hasErrors()) {
+			model.addObject("doc", doc);
+			model.setViewName("doc/form");
+			return model;
+		}
+
+		doc = docsRepository.save(doc);
+
+		model.setViewName(String.format("redirect:%d", doc.getId()));
+		return model;
+	}
+
+	@RequestMapping(value = "/new", method = RequestMethod.GET)
+	public ModelAndView create() {
+		ModelAndView model = new ModelAndView();
+		model.addObject("doc", new Doc());
+		model.setViewName("doc/form");
+		return model;
+	}
+
+
+
+	@RequestMapping(value = "/{id}/edit", method = RequestMethod.GET)
+	public ModelAndView update(@PathVariable long id) {
+		ModelAndView model = new ModelAndView();
+		model.addObject("doc", docsRepository.findById(id));
+		model.setViewName("doc/form");
+		return model;
+	}
+
+	@RequestMapping(value = "/{id}/delete")
+	public ModelAndView delete(@PathVariable long id) {
+		ModelAndView model = new ModelAndView();
+		docsRepository.deleteById(id);
+		model.setViewName("redirect:/doc/");
 		return model;
 	}
 }
