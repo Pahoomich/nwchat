5c4eb28d72dc8e372677a64555edcd0c65551093
idib <id.ib.mail@gmail.com>
2019-12-20 11:37:22 +0300

    test

--- src/test/java/com/nwchat/сontroller/OrderControllerTest.java
+++ src/test/java/com/nwchat/сontroller/OrderControllerTest.java
@@ -1,56 +1,66 @@
 package com.nwchat.сontroller;
 
+import com.nwchat.entity.OrderEntity;
 import com.nwchat.repository.testOrderRepo;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class OrderControllerTest {
-    OrderController orderController;
+	private OrderController orderController;
+	private testOrderRepo testOrderRepo;
 
-    @BeforeEach
-    void setUp() {
-        orderController = new OrderController(new testOrderRepo(),);
 
-    }
+	@BeforeEach
+	void setUp() {
+		testOrderRepo = new testOrderRepo();
+		orderController = new OrderController(testOrderRepo, );
 
-    @AfterEach
-    void tearDown() {
-    }
+	}
 
-    @Test
-    void index() {
+	@AfterEach
+	void tearDown() {
+	}
 
-    }
+	@Test
+	void index() {
 
-    @Test
-    void show() {
+	}
 
-        ModelAndView show = orderController.show(0);
-        assertEquals("order/show",show.getViewName());
-        assertEquals(show.getModel().get("userRole"));
-    }
+	@Test
+	void show() {
+		int id = 0;
 
-    @Test
-    void send() {
-    }
+		Optional<OrderEntity> expend = testOrderRepo.findById(id);
 
-    @Test
-    void save() {
-    }
 
-    @Test
-    void create() {
-    }
+		ModelAndView show = orderController.show(id);
+		assertEquals("order/show", show.getViewName());
+		assertEquals(expend, show.getModel().get("userRole"));
+	}
 
-    @Test
-    void update() {
-    }
+	@Test
+	void send() {
+	}
 
-    @Test
-    void delete() {
-    }
+	@Test
+	void save() {
+	}
+
+	@Test
+	void create() {
+	}
+
+	@Test
+	void update() {
+	}
+
+	@Test
+	void delete() {
+	}
 }
